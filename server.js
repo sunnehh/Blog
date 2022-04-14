78f8f84e893ded4610941316bd12e271ff757771
sunnehh <sunny_qi@live.ca>
2022-04-13 23:17:26 -0400

    moved directory

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static("./dist/bti425-a4-sunny"));
+app.use(express.static("./dist/bti425-a3-sunny"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("/index.html", { root: "dist/bti425-a4-sunny/" })
+  res.sendFile("/index.html", { root: "dist/bti425-a3-sunny/" })
 );
 
 // Start the app by listening on the default Heroku port
